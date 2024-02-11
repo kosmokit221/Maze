46f1e4413ed9bd11239590133dc093f9308b303b
kosmokit221 <144554376+kosmokit221@users.noreply.github.com>
2024-02-11 15:30:15 +0200

    Add files via upload

--- maze.py
+++ maze.py
@@ -2,6 +2,7 @@
 from random import choice
 from pygame import *
 mixer.init()
+font.init()
 
 #створи вікно гри
 TILESIZE = 45
@@ -9,6 +10,10 @@ MAP_WIDTH, MAP_HEIGHT = 19, 15
 WIDTH, HEIGHT = TILESIZE*MAP_WIDTH , TILESIZE*MAP_HEIGHT
 window = display.set_mode((WIDTH, HEIGHT))
 FPS = 60
+
+font1 = font.SysFont("Arial", 40)
+font2 = font.SysFont("Arial", 50)
+
 
 
 mixer.music.load("jungles.ogg")
@@ -27,8 +32,10 @@ bg = transform.scale(bg, (WIDTH, HEIGHT))
 player_img = image.load('hero.png')
 enemy_img = image.load('cyborg.png')
 wall_img = image.load('wall.png')
+treasure_img = image.load('treasure.png')
 
 #створи 2 спрайти та розмісти їх на сцені
+sprites = sprite.Group()
 class GameSprite(sprite.Sprite):
     def __init__(self, sprite_image, width, height, x, y):
         super().__init__()
@@ -36,19 +43,24 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        sprites.add(self)
 
     def draw(self, window):
         window.blit(self.image, self.rect)
 
 class Player(GameSprite):
     def __init__(self, sprite_image, width, height, x, y):
-        super().__init__(sprite_image, width, height, x, y)
+        super().__init__(sprite_image, width, height, x, y)   
         self.hp = 100
         self.damage = 20
         self.points = 0
         self.speed = 5
+    
 
     def update(self):
+        global hp_text
+        self.old_pos = self.rect.x, self.rect.y
+
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
@@ -58,6 +70,20 @@ class Player(GameSprite):
             self.rect.x -= self.speed
         if keys[K_d] and self.rect.right < WIDTH:
             self.rect.x += self.speed
+
+        collidelist = sprite.spritecollide(self, walls, False)
+        if len(collidelist) > 0:
+            self.rect.x, self.rect.y = self.old_pos
+        
+        collidelist = sprite.spritecollide(self, enemys, False)
+        if len(collidelist) >0:
+            self.hp -= 20
+            hp_text = font1.render(f"HP:{self.hp}", True, (255,255,255))
+            self.rect.x, self.rect.y = self.start_x, self.start_y
+            
+
+        
+
 enemys = sprite.Group()
 class Enemy(GameSprite):
     def __init__(self, x, y):
@@ -105,21 +131,33 @@ with open("map.txt", "r") as file:
             elif symbol == 'P':
                 player.rect.x = x
                 player.rect.y = y
+                player.start_x, player.start_y = x,y
+            elif symbol == "T":
+                GameSprite(treasure_img, TILESIZE, TILESIZE, x,y)
             x += TILESIZE
         y+= TILESIZE
         x = 0
+
+
+hp_text = font1.render(f"HP:{player.hp}", True, (255,255,255))
+finish_text = font2.render("GAME OVER", True, (255,0,0))
+finish = False
 
 while True:
 #оброби подію «клік за кнопкою "Закрити вікно"»
     for e in event.get():
         if e.type == QUIT:
             quit()
-    player.update()
-    enemys.update()
+    if not finish:
+        player.update()
+        enemys.update()
     window.blit(bg, (0,0))
-    player.draw(window)
-    walls.draw(window)
-    enemys.draw(window)
+    sprites.draw(window)
+    window.blit(hp_text, (10,10))
+    if player.hp <= 0:
+        finish = True
+    if finish:
+        window.blit(finish_text, (300,250))
     display.update()
     clock.tick(FPS)
 
